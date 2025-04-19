18f2f66647538ad90b03ab7def4f3d2d9e916fb1
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-19 18:01:11 +0530

    Added Comments to PrintMessage for better readibility and understanding of a viewer

--- src/threads/PrintMessage.java
+++ src/threads/PrintMessage.java
@@ -2,13 +2,19 @@ package threads;
 
 // Thread to print a message five times
 public class PrintMessage implements Runnable {
+
+    // Override the run() method to define the thread's task
     @Override
     public void run() {
+        // Loop to print the message five times
         for (int i = 1; i <= 5; i++) {
+            // Print the message along with the current count
             System.out.println("Message: Hello from Thread " + i);
             try {
+                // Pause the thread for 700 milliseconds
                 Thread.sleep(700);
             } catch (InterruptedException e) {
+                // Handle the exception if thread execution is interrupted
                 System.out.println("Message thread interrupted");
             }
         }
