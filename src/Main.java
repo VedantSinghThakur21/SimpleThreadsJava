12124a15d4a5174c7ab34a6a7e95e42450859d10
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-04-19 18:04:23 +0530

    Added Comments to Main.java for better readibility and understanding of a viewer

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,13 @@ package threads;
 public class Main {
     public static void main(String[] args) {
         // Creating instances of thread classes
-        Thread t1 = new Thread(new PrintNumbers());
-        Thread t2 = new Thread(new PrintCharacters());
-        Thread t3 = new Thread(new PrintMessage());
+        Thread t1 = new Thread(new PrintNumbers()); // Thread to print numbers from 1 to 5
+        Thread t2 = new Thread(new PrintCharacters()); // Thread to print characters A to E
+        Thread t3 = new Thread(new PrintMessage());  // Thread to print a message five times
 
         // Starting threads
-        t1.start();
-        t2.start();
-        t3.start();
+        t1.start(); // Starts number printing thread
+        t2.start(); // Starts character printing thread
+        t3.start(); // Starts message printing thread
     }
 }
